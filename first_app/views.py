ee1e4898766388efb7091cb9240053bd3fcb9efe
wbc <cjxx2280@126.com>
2022-04-20 14:12:58 +0800

    feat: switch to fake backend

--- first_app/views.py
+++ first_app/views.py
@@ -20,20 +20,21 @@ user_train = pd.read_csv(
 def predict_view(request):
     if request.method == 'POST':
         # user_test = pd.read_csv(tmp_filename, encoding="gbk")
-        user_test = request.FILES
-        predict_res_data = predict.predict_function(
-            user_train.copy(), user_test.copy())
-        feature_res_data = feature_importance.XGBoost_function(
-            user_train.copy(), user_test.copy())
+        # user_test = request.FILES
+        # predict_res_data = predict.predict_function(
+        #     user_train.copy(), user_test.copy())
+        # feature_res_data = feature_importance.XGBoost_function(
+        #     user_train.copy(), user_test.copy())
         # 传给前端 json
-        predict_json_data = predict_res_data.to_json(orient='records')
-        feature_json_data = feature_res_data.to_json(orient='records')
-        t = [{'predict': json.loads(
-            predict_json_data), 'feature_importance': json.loads(feature_json_data)}]
-        json_data = json.dumps(t)
-        text_content = json_data
-        # with open('predict_data.json') as f:
-        #     text_content = f.read()
+        # predict_json_data = predict_res_data.to_json(orient='records')
+        # feature_json_data = feature_res_data.to_json(orient='records')
+        # t = [{'predict': json.loads(
+        #     predict_json_data), 'feature_importance': json.loads(feature_json_data)}]
+        # json_data = json.dumps(t)
+        # text_content = json_data
+        text_content = None
+        with open('predict_data.json') as f:
+            text_content = f.read()
         return HttpResponse(text_content, content_type='application/json', charset='utf-8')
     else:
         return HttpResponse('方法错误')
@@ -41,16 +42,16 @@ def predict_view(request):
 
 def airplane_id_view(request):
     if request.method == 'POST':
-        air_id = request.POST.get('pass_id')
-        res_data = airplane_id.airplane_id_search(air_id)
+        # air_id = request.POST.get('pass_id')
+        # res_data = airplane_id.airplane_id_search(air_id)
 
         # 传给前端 json
-        json_data = res_data.to_json(orient='records')
-        text_content = json_data
+        # json_data = res_data.to_json(orient='records')
+        # text_content = json_data
 
-        # text_content = None
-        # with open('airplane_id.json') as f:
-        #     text_content = f.read()
+        text_content = None
+        with open('airplane_id.json') as f:
+            text_content = f.read()
         return HttpResponse(text_content, content_type='application/json', charset='utf-8')
     else:
         return HttpResponse('方法错误')
